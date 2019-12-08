33635f7f6121fbce6bfe9e8bb91de61683c28e5e
Faisal Rashid <faisalst32@live.com>
2019-12-08 21:05:05 +0530

    Added Carrom-Tac-Toe

---
+++ carrom-tac-toe/precache-manifest.823056c82a6b54476c7baeddd8a011a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ee6adae7423c9b1797dfdbc6830b1d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "807d18f938c43d7b4ec9",
+    "url": "/static/css/main.b82457bd.chunk.css"
+  },
+  {
+    "revision": "154442fb68748075c187",
+    "url": "/static/js/2.a6e4be0b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.a6e4be0b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "807d18f938c43d7b4ec9",
+    "url": "/static/js/main.2311de49.chunk.js"
+  },
+  {
+    "revision": "946db24c4e4d950c81b0",
+    "url": "/static/js/runtime-main.72440d5d.js"
+  },
+  {
+    "revision": "8c3d92f528bf0c89ff2b9bbbd12b0655",
+    "url": "/static/media/carrom-tac-toe-bg.8c3d92f5.jpg"
+  }
+]);
